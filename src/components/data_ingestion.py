055e01362b54fc6cf78e2a7eed2c9ddc75f02335
Jaswanth Madamanchi <jaswanth.m.ai@gmail.com>
2026-07-11 11:51:21 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,6 +4,10 @@ from src.components.exception import customException
 from src.components.logger import logging
 import pandas as pd
 
+from src.components.data_transformation import DataTransformation
+
+from src.components.data_transformation import dataTransformationConfig 
+
 from sklearn.model_selection import train_test_split
 
 from dataclasses import dataclass
@@ -48,7 +52,11 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion() 
+
+    data_transformation=DataTransformation()
+
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
 
 
